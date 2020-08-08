70735c8328b0407043976519ace809acdc9e3047
Nara Coldwater <coldw008@umn.edu>
2020-08-08 18:49:26 -0500

    outlined main, changed to tags

--- main.py
+++ main.py
@@ -5,3 +5,26 @@ with open('mock.json') as file:
     for task in data['tasks']:
         print(task['name'])
 
+def taskCreate(name):
+    pass
+
+def taskDelete(name):
+    pass
+
+def taskRename(name):
+    pass
+
+def taskTagAdd(name):
+    pass
+
+def taskTagRemove(name):
+    pass
+
+def tagCreate(name):
+    pass
+
+def tagDelete(name):
+    pass
+
+def tagRename(name):
+    pass
