a990343ec60a43ce0cae9f45c76e2050a5fed08a
Nara Coldwater <coldw008@umn.edu>
2020-08-13 15:19:18 -0500

    user can add text

--- tkinter-example.py
+++ tkinter-example.py
@@ -1,11 +1,18 @@
 import tkinter as tk
+from tkinter import * #change to specifics when I know what i want
 
 def set():
     print("set() run")
     labelText.set(labelText.get() + "+")
 
+def retrieve():
+    print(newTask.get())
+
 root = tk.Tk()
+root.geometry("900x500")
 root.title("ToDo")
+frame = tk.Frame(root)
+frame.pack()
 
 labelText = tk.StringVar()
 labelText.set("Initial Text")
@@ -14,5 +21,14 @@ label.pack()
 
 button = tk.Button(root, text = "Change Label Text", command = set)
 button.pack()
+
+newTask = Entry(root, width = 20)
+newTask.insert(0,'To-Do Item')
+newTask.pack(padx = 5, pady = 5)
+
+button = tk.Button(root, text = "add task", command = retrieve)
+button.pack()
+
+
 
 root.mainloop()
